e4e0780fec4f073a8b4fe454a7533b00efb2e48a
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-07-28 01:01:33 +0900

    no message

--- App.js
+++ App.js
@@ -5,11 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
