4431b872cb768e0833f48d754f9d524642e88e14
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-07-29 00:02:22 +0900

    no message

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!!!</Text>
+      <Text style={styles.text}>Hello!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  text: {
+      color: "white"
+  }
 });
