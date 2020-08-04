bee6846aa91f6b8d28b8d310ff831466537d0a35
DESKTOP-GSTOGUR\MJ <m_veloper@naver.com>
2020-08-04 23:48:37 +0900

    no message

--- Weather.js
+++ Weather.js
@@ -110,6 +110,7 @@ const styles = StyleSheet.create({
         color: "white",
         fontSize: 24
     },
+    
     textContainer: {
         paddingHorizontal: 20,
         alignItems: "flex-start"
